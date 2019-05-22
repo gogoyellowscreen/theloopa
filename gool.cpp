37c28ea96575f9f1700b1062f7bc72257162c751
gogoyellowscreen <roman.tihonov.2000@mail.ru>
2019-05-22 20:14:16 +0300

    secjnd branch

---
+++ gool.cpp
@@ -0,0 +1,5 @@
+//
+// Created by roman on 22.05.2019.
+//
+
+#include "gool.h"
---
+++ gool.h
@@ -0,0 +1,14 @@
+//
+// Created by roman on 22.05.2019.
+//
+
+#ifndef ITIVITI_SORT_GOOL_H
+#define ITIVITI_SORT_GOOL_H
+
+
+class gool {
+
+};
+
+
+#endif //ITIVITI_SORT_GOOL_H
